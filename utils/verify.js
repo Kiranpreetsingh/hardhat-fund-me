601283cfeb191da5ab2070678d878fed9eaafbee
unknown <skiranpreet8054@gmail.com>
2022-06-02 08:21:40 +0530

    initial commit

---
+++ utils/verify.js
@@ -0,0 +1,21 @@
+
+//verify is used for verify every deploy file: it will check is chainid is local or not
+const { run } = require("hardhat")
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+module.exports = { verify }
